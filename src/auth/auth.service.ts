8c5c95ccf20ac2ccb2f9cf3a8958c197e73a47f8
관해 (Jack) Park <jkhpark23@utexas.edu>
2021-05-21 16:53:37 +0900

    change auth module return types

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,13 @@
 import { Body, Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { CreateUserDto } from '../users/dto/create-user.dto';
+import { UserModel } from '../users/models/user.model';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly usersService: UsersService) {}
 
-  async register(@Body() userDto: CreateUserDto): Promise<any> {
+  async register(@Body() userDto: CreateUserDto): Promise<UserModel> {
     return await this.usersService.create(userDto);
   }
 }
