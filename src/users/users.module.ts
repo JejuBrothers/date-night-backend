101d7aca537d7832aa69fdd7e32c394c9bc01644
관해 (Jack) Park <jkhpark23@utexas.edu>
2021-06-24 16:26:08 +0900

    remove mail module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,13 +2,11 @@ import { Logger, Module } from '@nestjs/common';
 import { UsersRepository } from './users.repository';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { MailModule } from '../mail/mail.module';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from '../auth/guards/constants';
 
 @Module({
   imports: [
-    MailModule,
     JwtModule.register({
       secret: jwtConstants.secret,
       signOptions: { expiresIn: '1800s' },
